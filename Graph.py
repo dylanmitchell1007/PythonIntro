cd20fd3c5da3f3cfc878d5526c8847843717018c
dylanmitchell1007 <armadummy@yahoo.com>
2017-03-08 17:57:23 -0600

    3/8/2017

---
+++ Graph.py
@@ -0,0 +1,30 @@
+def func:
+    '''Functions'''
+
+if _name_ == "_main_"
+'''This is a way to create a simple class'''
+class Zombie(object):
+def __init__(self, hp, attack)
+self.health = hp
+self.attack = attack
+
+def attack(self, ninja):
+    ninja.takedamage(5)
+
+class Ninja(object):
+def __init__(self, hp, attack)
+self.health = hp
+self.attack = attack
+
+class Node(object)
+def __init__(self, value, id):
+    self.id = identifier
+    self.value = value
+
+class graph(object)
+def __init__(self, nodes):
+self.nodes = nodes
+def get_node(self, node):
+    '''get a node by an id'''
+    if node in self.nodes[identifier]
+        return node
